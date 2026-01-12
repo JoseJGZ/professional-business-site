fc9fbef4cbf1b915c6cd016cdf2d75b9ba4099c6
JoseJGZ <josejuliangz@gmail.com>
2026-01-12 05:21:23 -0600

    feat: add home page content

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,21 +1,18 @@
 import { Link } from "react-router-dom";
+
 function Navbar() {
   return (
-    <nav className="fixed w-full z-50 bg-white/80 backdrop-blur-md border-b border-slate-100">
-      <div className="max-w-6xl mx-auto px-6 h-20 flex items-center justify-center text-white font-black">
-        <Link
-          to="/"
-          className="flex items-center gap-2 hover:opacity-80 transition-opacity"
-        >
-          <div className="w-10 h-10 bg-blue-600 rounded-xl flex items-center justify-center text-white font-black">
-            E
+    <nav className="fixed top-0 w-full z-50 bg-white/80 backdrop-blur-md border-b border-slate-100">
+      <div className="max-w-6xl mx-auto px-6 h-20 flex items-center justify-between">
+        <Link to="/" className="flex items-center gap-2">
+          <div className="w-10 h-10 bg-blue-600 rounded-xl flex items-center justify-center text-white font-black text-xl">
+            E-L
           </div>
-          <span className="text-xl font-bold tracking-tight text-slate-900">
+          <span className="text-xl font-bold text-slate-900 tracking-tight">
             EasyLoan
           </span>
         </Link>
-
-        <div className="hidden md:flex items-center gap-8 text-sm font-medium text-slate-600">
+        <div className="hidden md:flex items-center gap-12 text-lg font-medium text-slate-600">
           <Link
             to="/services"
             className="hover:text-blue-600 transition-colors"
@@ -30,14 +27,12 @@ function Navbar() {
           </Link>
         </div>
 
-        <div>
-          <Link
-            to="/Contact"
-            className="bg-slate-900 text-white px-6 py-2.5 rounded-full text-sm font-bold hover:bg-blue-600 transition-all shadow-sm"
-          >
-            Apply Now
-          </Link>
-        </div>
+        <Link
+          to="/contact"
+          className="bg-slate-900 text-white px-6 py-2.5 rounded-full text-sm font-bold hover:bg-blue-600 transition-all shadow-sm"
+        >
+          Apply Now
+        </Link>
       </div>
     </nav>
   );
