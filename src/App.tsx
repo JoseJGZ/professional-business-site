84545d10d333746ce2ea60fb02efd026da9ba9ed
JoseJGZ <josejuliangz@gmail.com>
2026-01-12 04:35:56 -0600

    feat: add home page content

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
+import Home from "./pages/Home";
+
 function App() {
   return (
-    <h1 className="text-3xl font-bold text-blue-600">Proyecto Profesional</h1>
+    <main>
+      <Home />
+    </main>
   );
 }
 export default App;
